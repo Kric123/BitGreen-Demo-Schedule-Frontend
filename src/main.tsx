0e0998a954541994df5b1358dd99afaa0ab5cbcd
Kyle Riccardi <Kylericcardi99@gmail.com>
2026-03-29 16:57:04 -0400

    disabled content restrictions

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
+   <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
     <App />
   </BrowserRouter>
 )
