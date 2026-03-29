e8b85268d448dc247f9a0b92239b29c75c5c7c75
Kyle Riccardi <Kylericcardi99@gmail.com>
2026-03-29 17:24:45 -0400

    fixed an issue locally

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,13 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://bgbackenddemo-env.eba-pucdzufm.us-east-1.elasticbeanstalk.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    }
+  }
 })
